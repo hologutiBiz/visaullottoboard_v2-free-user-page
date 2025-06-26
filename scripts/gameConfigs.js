499cfbf3649cdd6a9022680aff757b045f130490
Akin Holo <holoakinseloyin@gmail.com>
2025-06-26 12:25:49 +0100

    renaumber games and added more

--- scripts/gameConfigs.js
+++ scripts/gameConfigs.js
@@ -6,21 +6,41 @@ export const gameConfigs = [
     slug: '1diamond',
     color: '#fcd000', // optional for styling
   },
+  {
+    key: 'msp',
+    label: 'Monday Special',
+    slug: '18msp.html',
+},
+  {
+    key: 'lucky-g',
+    label: 'Lucky-G',
+    slug: '19lucky-g',
+},
 {
     key: 'midweek',
     label: 'Midweek',
-    slug: '21midweek',
-  },
+    slug: '20midweek',
+},
 {
     key: 'fortune',
     label: 'Fortune',
-    slug: '22fortune',
+    slug: '21fortune',
   },
   {
+    key: 'bonanza',
+    label: 'Bonanza',
+    slug: '22bonanza.html',
+},
+{
     key: 'national',
     label: 'National',
     slug: '24national',
-  }
+},
+{
+    key: 'aseda',
+    label: 'Aseda',
+    slug: '25aseda.html',
+},
  
   // Add more games here
 ];
